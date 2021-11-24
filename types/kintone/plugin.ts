9cd90c3c8584cb75189cb3a9eb2969d6420320c3
shintaroNagata <cybozu.nagata@gmail.com>
2021-11-25 00:29:35 +0900

    :sparkles: feat: refine the definitions for `kintone.proxy` & `kintone.plugin`

--- types/kintone/plugin.ts
+++ types/kintone/plugin.ts
@@ -1,34 +1,73 @@
 declare global {
   namespace kintone.plugin.app {
-    function setConfig(config: any, successCallback?: () => any): void;
-    function getConfig(pluginId: string): any;
+    function setConfig(
+      config: { [Key in string]?: string },
+      successCallback?: () => void
+    ): void;
+    function getConfig(pluginId: string): { [Key in string]?: string };
     function proxy(
       pluginId: string,
       url: string,
       method: "GET" | "POST" | "PUT" | "DELETE",
-      headers: any,
-      data: any
-    ): Promise<any>;
+      headers: { [Key: string]: string },
+      data: { [Key: string]: string } | string
+    ): Promise<[string, number, { [Key in string]?: string }]>;
+    function proxy(
+      pluginId: string,
+      url: string,
+      method: "GET" | "POST" | "PUT" | "DELETE",
+      headers: { [Key: string]: string },
+      data: { [Key: string]: string } | string,
+      successCallback: (
+        responseBody: string,
+        statusCode: number,
+        responseHeader: { [Key in string]?: string }
+      ) => void,
+      failureCallback?: (errorResponseBody: string) => void
+    ): void;
+
     function setProxyConfig(
       url: string,
       method: "GET" | "POST" | "PUT" | "DELETE",
-      headers: any,
-      data: any,
-      successCallback?: () => any
+      headers: { [Key: string]: string },
+      data: { [Key: string]: string },
+      successCallback?: () => void
     ): void;
     function getProxyConfig(
       url: string,
       method: "GET" | "POST" | "PUT" | "DELETE"
-    ): any;
+    ): {
+      headers: { [Key in string]?: string };
+      data: { [Key in string]?: string };
+    };
   }
   namespace kintone.plugin.app.proxy {
     function upload(
       pluginId: string,
       url: string,
       method: "POST" | "PUT",
-      headers: any,
-      data: any
-    ): Promise<any>;
+      headers: { [Key: string]: string },
+      data: {
+        format: "RAW";
+        value: unknown;
+      }
+    ): Promise<[string, number, { [Key in string]?: string }]>;
+    function upload(
+      pluginId: string,
+      url: string,
+      method: "POST" | "PUT",
+      headers: { [Key: string]: string },
+      data: {
+        format: "RAW";
+        value: unknown;
+      },
+      successCallback: (
+        responseBody: string,
+        statusCode: number,
+        responseHeader: { [Key in string]?: string }
+      ) => void,
+      failureCallback?: (errorResponseBody: string) => void
+    ): void;
   }
 }
 
--- types/kintone/proxy.ts
+++ types/kintone/proxy.ts
@@ -3,17 +3,48 @@ declare global {
     function proxy(
       url: string,
       method: "GET" | "POST" | "PUT" | "DELETE",
-      headers: any,
-      data: any
-    ): Promise<any>;
+      headers: { [Key: string]: string },
+      data: { [Key: string]: string } | string
+    ): Promise<[string, number, { [Key in string]?: string }]>;
+
+    function proxy(
+      url: string,
+      method: "GET" | "POST" | "PUT" | "DELETE",
+      headers: { [Key: string]: string },
+      data: { [Key: string]: string } | string,
+      successCallback: (
+        responseBody: string,
+        statusCode: number,
+        responseHeader: { [Key in string]?: string }
+      ) => void,
+      failureCallback?: (errorResponseBody: string) => void
+    ): void;
   }
   namespace kintone.proxy {
     function upload(
       url: string,
       method: "POST" | "PUT",
-      headers: any,
-      data: any
-    ): Promise<any>;
+      headers: { [Key: string]: string },
+      data: {
+        format: "RAW";
+        value: unknown;
+      }
+    ): Promise<[string, number, { [Key in string]?: string }]>;
+    function upload(
+      url: string,
+      method: "POST" | "PUT",
+      headers: { [Key: string]: string },
+      data: {
+        format: "RAW";
+        value: unknown;
+      },
+      successCallback: (
+        responseBody: string,
+        statusCode: number,
+        responseHeader: { [Key in string]?: string }
+      ) => void,
+      failureCallback?: (errorResponseBody: string) => void
+    ): void;
   }
 }
 
