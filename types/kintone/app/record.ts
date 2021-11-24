5c296a6de6751933cbdec59da87841633087e70d
shintaroNagata <cybozu.nagata@gmail.com>
2021-11-24 23:53:32 +0900

    :sparkles: feat: refine the difinitions for `kintone.app`

--- types/kintone/app/record.ts
+++ types/kintone/app/record.ts
@@ -1,10 +1,12 @@
+import { KintoneRecord } from "../types";
+
 declare global {
   namespace kintone.app.record {
     function getId(): number | null;
-    function get(): any;
-    function set(record: any): void;
-    function setFieldShown(fieldCode: string, isShown: boolean): any;
-    function setGroupFieldOpen(fieldCode: string, isOpen: boolean): any;
+    function get(): KintoneRecord;
+    function set(record: KintoneRecord): void;
+    function setFieldShown(fieldCode: string, isShown: boolean): void;
+    function setGroupFieldOpen(fieldCode: string, isOpen: boolean): void;
     function getFieldElement(fieldCode: string): HTMLElement | null;
     function getHeaderMenuSpaceElement(): HTMLElement | null;
     function getSpaceElement(id: string): HTMLElement | null;
--- types/kintone/mobile/app/record.ts
+++ types/kintone/mobile/app/record.ts
@@ -1,10 +1,12 @@
+import { KintoneRecord } from "../../types";
+
 declare global {
   namespace kintone.mobile.app.record {
     function getId(): number | null;
-    function get(): any;
-    function set(record: any): void;
-    function setFieldShown(fieldCode: string, isShown: boolean): any;
-    function setGroupFieldOpen(fieldCode: string, isOpen: boolean): any;
+    function get(): KintoneRecord;
+    function set(record: KintoneRecord): void;
+    function setFieldShown(fieldCode: string, isShown: boolean): void;
+    function setGroupFieldOpen(fieldCode: string, isOpen: boolean): void;
     function getFieldElement(fieldCode: string): HTMLElement | null;
     function getSpaceElement(id: string): HTMLElement | null;
   }
