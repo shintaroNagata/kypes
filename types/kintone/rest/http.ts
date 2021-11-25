ff6232764fd671050f5d0d31e909e7704fb78403
shintaroNagata <cybozu.nagata@gmail.com>
2021-11-25 15:47:08 +0900

    :sparkles: feat: add a url pattern

--- types/kintone/rest/http.ts
+++ types/kintone/rest/http.ts
@@ -4,7 +4,8 @@ type PathFor<Endpoint extends string> = `/k/v1/${Endpoint}`;
 type Domain = "cybozu.com" | "kintone.com" | "cybozu.cn";
 type UrlFor<Endpoint extends string> =
   | `https://${string}.${Domain}/k/v1/${Endpoint}.json`
-  | `https://${string}.${Domain}/k/guest/${number}/v1/${Endpoint}.json`;
+  | `https://${string}.${Domain}/k/guest/${number}/v1/${Endpoint}.json`
+  | `https://${string}.${Domain}/k/guest/${number}/v1/${Endpoint}.json?${string}`;
 
 type EndpointOf<PathOrUrl extends PathFor<string> | UrlFor<string>> =
   PathOrUrl extends PathFor<infer Endpoint>
