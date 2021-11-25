e8c4fe2d218167876cf655d2ec336fab9a8b8923
shintaroNagata <cybozu.nagata@gmail.com>
2021-11-26 06:14:14 +0900

    :sparkles: feat: add type definitions for `/settings`

--- types/kintone/rest/app/settings.ts
+++ types/kintone/rest/app/settings.ts
@@ -2,20 +2,113 @@ type SettingsRestApiMap = {
   GetAppSettings: {
     method: "GET";
     endpoint: "app/settings";
-    requestParameters: any;
-    responseProperties: any;
+    requestParameters: {
+      app: string | number;
+      lang?: "ja" | "en" | "zh" | "user" | "default";
+    };
+    responseProperties: {
+      name: string;
+      description: string;
+      icon:
+        | {
+            type: "FILE";
+            file: {
+              contentType: string;
+              fileKey: string;
+              name: string;
+              size: string;
+            };
+          }
+        | {
+            type: "PRESET";
+            key: string;
+          };
+      theme:
+        | "WHITE"
+        | "RED"
+        | "BLUE"
+        | "GREEN"
+        | "YELLOW"
+        | "BLACK"
+        | "CLIPBOARD"
+        | "BINDER"
+        | "PENCIL"
+        | "CLIPS";
+      revision: string;
+    };
   };
   GetPreviewAppSettings: {
     method: "GET";
     endpoint: "preview/app/settings";
-    requestParameters: any;
-    responseProperties: any;
+    requestParameters: {
+      app: string | number;
+      lang?: "ja" | "en" | "zh" | "user" | "default";
+    };
+    responseProperties: {
+      name: string;
+      description: string;
+      icon:
+        | {
+            type: "FILE";
+            file: {
+              contentType: string;
+              fileKey: string;
+              name: string;
+              size: string;
+            };
+          }
+        | {
+            type: "PRESET";
+            key: string;
+          };
+      theme:
+        | "WHITE"
+        | "RED"
+        | "BLUE"
+        | "GREEN"
+        | "YELLOW"
+        | "BLACK"
+        | "CLIPBOARD"
+        | "BINDER"
+        | "PENCIL"
+        | "CLIPS";
+      revision: string;
+    };
   };
   PutPreviewAppSettings: {
     method: "PUT";
     endpoint: "preview/app/settings";
-    requestParameters: any;
-    responseProperties: any;
+    requestParameters: {
+      app: string | number;
+      name?: string;
+      description?: string;
+      icon?:
+        | {
+            type: "FILE";
+            file: {
+              fileKey: string;
+            };
+          }
+        | {
+            type: "PRESET";
+            key: string;
+          };
+      theme?:
+        | "WHITE"
+        | "RED"
+        | "BLUE"
+        | "GREEN"
+        | "YELLOW"
+        | "BLACK"
+        | "CLIPBOARD"
+        | "BINDER"
+        | "PENCIL"
+        | "CLIPS";
+      revision?: string | number;
+    };
+    responseProperties: {
+      revision: string;
+    };
   };
 };
 
