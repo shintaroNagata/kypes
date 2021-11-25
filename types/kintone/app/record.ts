4b3f4101b715bc106883083dd638b5264154150b
shintaroNagata <cybozu.nagata@gmail.com>
2021-11-25 15:42:45 +0900

    :bug: fix: modify the definitions for `kintone.app.record.get()` & `kintone.app.record.set()`

--- types/kintone/app/record.ts
+++ types/kintone/app/record.ts
@@ -3,8 +3,8 @@ import { KintoneRecord } from "../types";
 declare global {
   namespace kintone.app.record {
     function getId(): number | null;
-    function get(): KintoneRecord;
-    function set(record: KintoneRecord): void;
+    function get(): { record: KintoneRecord };
+    function set(recordObject: { record: KintoneRecord }): void;
     function setFieldShown(fieldCode: string, isShown: boolean): void;
     function setGroupFieldOpen(fieldCode: string, isOpen: boolean): void;
     function getFieldElement(fieldCode: string): HTMLElement | null;
