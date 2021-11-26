4a542e121849fa17f21c21747596b639b1bb61e1
shintaroNagata <cybozu.nagata@gmail.com>
2021-11-26 12:36:01 +0900

    :sparkles: feat: add type definitions for `/status`

--- types/kintone/rest/app/status.ts
+++ types/kintone/rest/app/status.ts
@@ -2,20 +2,135 @@ type StatusRestApiMap = {
   GetAppStatus: {
     method: "GET";
     endpoint: "app/status";
-    requestParameters: any;
-    responseProperties: any;
+    requestParameters: {
+      app: string | number;
+      lang?: "ja" | "en" | "zh" | "user" | "default";
+    };
+    responseProperties: {
+      enable: boolean;
+      states: {
+        [stateName: string]:
+          | {
+              name: string;
+              index: string;
+              assignee: {
+                type: "ONE" | "ALL" | "ANY";
+                entities: Array<{
+                  entity:
+                    | {
+                        type:
+                          | "USER"
+                          | "GROUP"
+                          | "ORGANIZATION"
+                          | "FIELD_ENTITY"
+                          | "CUSTOM_FIELD";
+                        code: string;
+                      }
+                    | {
+                        type: "CREATOR";
+                        code: null;
+                      };
+                  includeSubs: boolean;
+                }>;
+              };
+            }
+          | undefined;
+      } | null;
+      actions: Array<{
+        name: string;
+        from: string;
+        to: string;
+        filterCond: string;
+      }>;
+      revision: string;
+    };
   };
   GetPreviewAppStatus: {
     method: "GET";
     endpoint: "preview/app/status";
-    requestParameters: any;
-    responseProperties: any;
+    requestParameters: {
+      app: string | number;
+      lang?: "ja" | "en" | "zh" | "user" | "default";
+    };
+    responseProperties: {
+      enable: boolean;
+      states: {
+        [stateName: string]:
+          | {
+              name: string;
+              index: string;
+              assignee: {
+                type: "ONE" | "ALL" | "ANY";
+                entities: Array<{
+                  entity:
+                    | {
+                        type:
+                          | "USER"
+                          | "GROUP"
+                          | "ORGANIZATION"
+                          | "FIELD_ENTITY"
+                          | "CUSTOM_FIELD";
+                        code: string;
+                      }
+                    | {
+                        type: "CREATOR";
+                        code: null;
+                      };
+                  includeSubs: boolean;
+                }>;
+              };
+            }
+          | undefined;
+      } | null;
+      actions: Array<{
+        name: string;
+        from: string;
+        to: string;
+        filterCond: string;
+      }>;
+      revision: string;
+    };
   };
   PutPreviewAppStatus: {
     method: "PUT";
     endpoint: "preview/app/status";
-    requestParameters: any;
-    responseProperties: any;
+    requestParameters: {
+      app: string | number;
+      enable?: boolean;
+      states?: {
+        [stateName: string]: {
+          name?: string;
+          index: string;
+          assignee?: {
+            type: "ONE" | "ALL" | "ANY";
+            entities: Array<{
+              entity:
+                | {
+                    type:
+                      | "USER"
+                      | "GROUP"
+                      | "ORGANIZATION"
+                      | "FIELD_ENTITY"
+                      | "CUSTOM_FIELD";
+                    code: string;
+                  }
+                | {
+                    type: "CREATOR";
+                  };
+              includeSubs?: boolean;
+            }>;
+          };
+        };
+      };
+      actions?: Array<{
+        name: string;
+        from: string;
+        to: string;
+        filterCond?: string;
+      }>;
+      revision?: string | number;
+    };
+    responseProperties: { revision: string };
   };
 };
 
