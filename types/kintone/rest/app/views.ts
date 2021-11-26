ed20efd9ba4bbc7aad80898742e5e8863d08819a
shintaroNagata <cybozu.nagata@gmail.com>
2021-11-26 12:48:06 +0900

    :sparkles: feat: add type definitions for `/views`

--- types/kintone/rest/app/views.ts
+++ types/kintone/rest/app/views.ts
@@ -2,20 +2,121 @@ type ViewsRestApiMap = {
   GetAppViews: {
     method: "GET";
     endpoint: "app/views";
-    requestParameters: any;
-    responseProperties: any;
+    requestParameters: {
+      app: string | number;
+      lang?: "ja" | "en" | "zh" | "user" | "default";
+    };
+    responseProperties: {
+      views: {
+        [viewName: string]:
+          | ({
+              name: string;
+              id: string;
+              index: string;
+              filterCond: string;
+              sort: string;
+            } & (
+              | {
+                  type: "LIST";
+                  builtinType: "ASSIGNEE" | undefined;
+                  fields: string[];
+                }
+              | {
+                  type: "CALENDAR";
+                  date: string;
+                  title: string;
+                }
+              | {
+                  type: "CUSTOM";
+                  html: string;
+                  pager: boolean;
+                  device: "DESKTOP" | "ANY";
+                }
+            ))
+          | undefined;
+      };
+      revision: string;
+    };
   };
   GetPreviewAppViews: {
     method: "GET";
     endpoint: "preview/app/views";
-    requestParameters: any;
-    responseProperties: any;
+    requestParameters: {
+      app: string | number;
+      lang?: "ja" | "en" | "zh" | "user" | "default";
+    };
+    responseProperties: {
+      views: {
+        [viewName: string]:
+          | ({
+              name: string;
+              id: string;
+              index: string;
+              filterCond: string;
+              sort: string;
+            } & (
+              | {
+                  type: "LIST";
+                  builtinType: "ASSIGNEE" | undefined;
+                  fields: string[];
+                }
+              | {
+                  type: "CALENDAR";
+                  date: string;
+                  title: string;
+                }
+              | {
+                  type: "CUSTOM";
+                  html: string;
+                  pager: boolean;
+                  device: "DESKTOP" | "ANY";
+                }
+            ))
+          | undefined;
+      };
+      revision: string;
+    };
   };
   PutPreviewAppViews: {
     method: "PUT";
     endpoint: "preview/app/views";
-    requestParameters: any;
-    responseProperties: any;
+    requestParameters: {
+      app: string | number;
+      views: {
+        [viewName: string]: {
+          name?: string;
+          id: string;
+          index: string;
+          filterCond?: string;
+          sort?: string;
+        } & (
+          | {
+              type: "LIST";
+              fields?: string[];
+            }
+          | {
+              type: "CALENDAR";
+              date?: string;
+              title?: string;
+            }
+          | {
+              type: "CUSTOM";
+              html?: string;
+              pager?: boolean;
+              device?: "DESKTOP" | "ANY";
+            }
+        );
+      };
+      revision?: string | number;
+    };
+    responseProperties: {
+      revision: string;
+      views: {
+        [viewName: string]: {
+          id: string;
+        };
+      };
+    };
   };
 };
 
