2cda5a33788e54ff220c06bd1d89299be8b74fb6
shintaroNagata <cybozu.nagata@gmail.com>
2021-11-26 12:11:26 +0900

    :sparkles: feat: add type definitions for `/actions`

--- types/kintone/rest/app/actions.ts
+++ types/kintone/rest/app/actions.ts
@@ -2,20 +2,123 @@ type ActionsRestApiMap = {
   GetAppActions: {
     method: "GET";
     endpoint: "app/actions";
-    requestParameters: any;
-    responseProperties: any;
+    requestParameters: {
+      app: string | number;
+      lang?: "ja" | "en" | "zh" | "user" | "default";
+    };
+    responseProperties: {
+      actions: {
+        [actionName: string]:
+          | {
+              name: string;
+              id: string;
+              index: string;
+              destApp: {
+                app: string;
+                code: string;
+              };
+              mapping: Array<
+                | {
+                    srcType: "FIELD";
+                    srcField: string;
+                    destField: string;
+                  }
+                | {
+                    srcType: "RECORD_URL";
+                    destField: string;
+                  }
+              >;
+              entities: Array<{
+                type: "USER" | "GROUP" | "ORGANIZATION";
+                code: string;
+              }>;
+            }
+          | undefined;
+      };
+      revision: string;
+    };
   };
   GetPreviewAppActions: {
     method: "GET";
     endpoint: "preview/app/actions";
-    requestParameters: any;
-    responseProperties: any;
+    requestParameters: {
+      app: string | number;
+      lang?: "ja" | "en" | "zh" | "user" | "default";
+    };
+    responseProperties: {
+      actions: {
+        [actionName: string]:
+          | {
+              name: string;
+              id: string;
+              index: string;
+              destApp: {
+                app: string;
+                code: string;
+              };
+              mapping: Array<
+                | {
+                    srcType: "FIELD";
+                    srcField: string;
+                    destField: string;
+                  }
+                | {
+                    srcType: "RECORD_URL";
+                    destField: string;
+                  }
+              >;
+              entities: Array<{
+                type: "USER" | "GROUP" | "ORGANIZATION";
+                code: string;
+              }>;
+            }
+          | undefined;
+      };
+      revision: string;
+    };
   };
   PutPreviewAppActions: {
     method: "PUT";
     endpoint: "preview/app/actions";
-    requestParameters: any;
-    responseProperties: any;
+    requestParameters: {
+      app: string | number;
+      actions: {
+        [actionName: string]: {
+          name?: string;
+          index: string;
+          destApp?: {
+            app?: string;
+            code?: string;
+          };
+          mapping?: Array<
+            | {
+                srcType: "FIELD";
+                srcField: string;
+                destField: string;
+              }
+            | {
+                srcType: "RECORD_URL";
+                destField: string;
+              }
+          >;
+          entities?: Array<{
+            type: "USER" | "GROUP" | "ORGANIZATION";
+            code: string;
+          }>;
+        };
+      };
+      revision?: string | number;
+    };
+    responseProperties: {
+      actions: {
+        [actionName: string]:
+          | {
+              id: string;
+            }
+          | undefined;
+      };
+      revision: string;
+    };
   };
 };
 
