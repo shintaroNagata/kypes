83f455a20e748d9fb8ccae90e58fa95ef2007bee
shintaroNagata <cybozu.nagata@gmail.com>
2021-12-10 19:36:53 +0900

    :bug: fix: return type of `kintone.app.record.get()`

--- types/kintone/app/record.ts
+++ types/kintone/app/record.ts
@@ -3,7 +3,7 @@ import { KintoneRecord } from "../types";
 declare global {
   namespace kintone.app.record {
     function getId(): number | null;
-    function get(): { record: KintoneRecord };
+    function get(): { record: KintoneRecord } | null;
     function set(recordObject: { record: KintoneRecord }): void;
     function setFieldShown(fieldCode: string, isShown: boolean): void;
     function setGroupFieldOpen(fieldCode: string, isOpen: boolean): void;
