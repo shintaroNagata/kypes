7218d21f34b1c21e47826a17184e608784a7a5f5
shintaroNagata <cybozu.nagata@gmail.com>
2022-10-27 21:00:37 +0900

    :fire: refactor: remove old type definitions

--- types/rest/temporal.ts
+++
@@ -1,6 +0,0 @@
-import { FindApi, EnableMethods } from "./schema";
-
-type Parameters<Endpoint, Method> = FindApi<Endpoint, Method>["parameters"];
-type Response<Endpoint, Method> = FindApi<Endpoint, Method>["response"];
-
-export { Parameters, Response, EnableMethods };
