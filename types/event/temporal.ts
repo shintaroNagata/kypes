c5db948419e0a89663b7055cfe1a24aac3264c66
shintaroNagata <cybozu.nagata@gmail.com>
2022-10-29 22:41:07 +0900

    :building_construction: refactor: replace `Event` type

--- types/event/temporal.ts
+++
@@ -1,9 +0,0 @@
-import { Properties as EventProperties } from "./property";
-
-type EventTypes = keyof EventProperties;
-
-type Event<EventType extends EventTypes> = EventType extends unknown
-  ? { type: EventType } & EventProperties[EventType]
-  : never;
-
-export { Event, EventTypes };
