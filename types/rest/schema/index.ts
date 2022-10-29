cbfa7cf0f1461b2d07cdf6d00e45e8dc69745b40
shintaroNagata <cybozu.nagata@gmail.com>
2022-10-29 19:03:50 +0900

    :bug: fix: modify `FindApi` to return correct union types

--- types/rest/schema/index.ts
+++ types/rest/schema/index.ts
@@ -1,6 +1,5 @@
 import { Endpoints } from "../http";
 import { ApiSchema } from "./types";
-import { KeyOfUnion } from "./utility";
 import { Schema as RecordApiSchema } from "./record";
 import { Schema as BulkRequestApiSchema } from "./bulkRequest";
 import { Schema as AppApiSchema } from "./app";
@@ -15,10 +14,12 @@ type GetEndpointSchema<Endpoint> = Endpoint extends Endpoints
   ? KintoneRestApiSchema[Endpoint]
   : never;
 
-type EnableMethods<Endpoint> = KeyOfUnion<GetEndpointSchema<Endpoint>>;
+type EnableMethods<Endpoint> = keyof GetEndpointSchema<Endpoint>;
 
-type FindApi<Endpoint, Method> = Method extends EnableMethods<Endpoint>
-  ? Extract<GetEndpointSchema<Endpoint>[Method], ApiSchema>
+type FindApi<Endpoint, Method> = Endpoint extends unknown
+  ? Method extends EnableMethods<Endpoint>
+    ? Extract<GetEndpointSchema<Endpoint>[Method], ApiSchema>
+    : never
   : never;
 
 export { FindApi, EnableMethods };
