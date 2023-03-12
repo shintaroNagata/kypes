d65060bac8ade8016765069ee32f4b8c7727da0b
shintaroNagata <cybozu.nagata@gmail.com>
2023-03-13 00:25:51 +0900

    :building_construction: refactor: move `/rest` -> `kypes-rest`

---
+++ packages/kypes-rest/types/index.d.ts
@@ -0,0 +1,5 @@
+import type { ApiSchema } from "./types";
+import type { Url, QueriedUrl } from "./url";
+import type * as KintoneApi from "./kintone";
+
+export type { ApiSchema, Url, QueriedUrl, KintoneApi };
--- packages/kypes/types/rest/index.d.ts
+++
@@ -1,5 +0,0 @@
-import type { ApiSchema } from "./types";
-import type { Url, QueriedUrl } from "./url";
-import type * as KintoneApi from "./kintone";
-
-export type { ApiSchema, Url, QueriedUrl, KintoneApi };
